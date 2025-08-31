4960c01198bffd2d949124cb0055630d25556cad
nickchong0 <nicholas.m.chong@gmail.com>
2025-08-31 15:31:55 -0400

    fix placeholder imgs

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**',
+      },
     ],
   },
 };
